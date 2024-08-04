368a3a9a93a28df9ffdf3e8204696811ac7a45bd
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2024-08-04 10:28:24 +0200

    fix: fixes

--- builder/download_models.py
+++ builder/download_models.py
@@ -1,27 +1,25 @@
 from transformers import AutoTokenizer, AutoModel
 import os
 
-print("Lul")
+def download_model(model_path, model_name):
+    print("Downloading {model_name} to {model_path}")
+    """Download a Hugging Face model and tokenizer to the specified directory"""
+    # Check if the directory already exists
+    if not os.path.exists(model_path):
+        # Create the directory
+        os.makedirs(model_path)
 
-# def download_model(model_path, model_name):
-#     print("Downloading {model_name} to {model_path}")
-#     """Download a Hugging Face model and tokenizer to the specified directory"""
-#     # Check if the directory already exists
-#     if not os.path.exists(model_path):
-#         # Create the directory
-#         os.makedirs(model_path)
+    print("Downloading model...")
+    model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    print("Downloading tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
-#     print("Downloading model...")
-#     model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
-#     print("Downloading tokenizer...")
-#     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    # Save the model and tokenizer to the specified directory
+    print("Saving model...")
+    model.save_pretrained(model_path)
+    print("Saving tokenizer...")
+    tokenizer.save_pretrained(model_path)
 
-#     # Save the model and tokenizer to the specified directory
-#     print("Saving model...")
-#     model.save_pretrained(model_path)
-#     print("Saving tokenizer...")
-#     tokenizer.save_pretrained(model_path)
+    print("Download finished.")
 
-#     print("Download finished.")
-
-# download_model('model/', 'sdasd112132/Vision-8B-MiniCPM-2_5-Uncensored-and-Detailed-4bit')
+download_model('model/', 'sdasd112132/Vision-8B-MiniCPM-2_5-Uncensored-and-Detailed-4bit')
